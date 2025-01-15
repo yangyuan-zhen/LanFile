bb7a676755fefdf22f76ae57dc8f1a6f6dbf99f3
2569718930@qq.com <2569718930@qq.com>
2025-01-15 21:54:10 +0800

    refactor: replace WebSocket implementation with SimplePeer for P2P file transfer; remove WebSocketServer and WebSocketClient classes, update dependencies in package.json and package-lock.json

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,9 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
-import { WebSocketServer } from './services/WebSocketServer';
 
 let mainWindow: BrowserWindow | null = null;
-let wsServer: WebSocketServer | null = null;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
@@ -23,24 +21,8 @@ function createWindow() {
         mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
     }
 
-    // 初始化WebSocket服务器
-    wsServer = new WebSocketServer(mainWindow);
-
-    // 将服务器信息发送给渲染进程
-    const serverInfo = {
-        port: wsServer.getPort(),
-        addresses: wsServer.getLocalIPs()
-    };
-    mainWindow.webContents.on('did-finish-load', () => {
-        mainWindow?.webContents.send('server-info', serverInfo);
-    });
-
     mainWindow.on('closed', () => {
         mainWindow = null;
-        if (wsServer) {
-            wsServer.close();
-            wsServer = null;
-        }
     });
 }
 
--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,27 +1,6 @@
-import { contextBridge, ipcRenderer } from 'electron';
+import { contextBridge } from 'electron';
 
 // 暴露给渲染进程的API
 contextBridge.exposeInMainWorld('electron', {
-    // 接收服务器信息
-    onServerInfo: (callback: (serverInfo: any) => void) => {
-        ipcRenderer.on('server-info', (_event, serverInfo) => callback(serverInfo));
-    },
-
-    // 文件传输相关事件
-    onTransferStarted: (callback: (data: any) => void) => {
-        ipcRenderer.on('transfer-started', (_event, data) => callback(data));
-    },
-
-    onChunkReceived: (callback: (chunk: any) => void) => {
-        ipcRenderer.on('chunk-received', (_event, chunk) => callback(chunk));
-    },
-
-    onTransferComplete: (callback: (data: any) => void) => {
-        ipcRenderer.on('transfer-complete', (_event, data) => callback(data));
-    },
-
-    // 移除事件监听
-    removeAllListeners: (channel: string) => {
-        ipcRenderer.removeAllListeners(channel);
-    }
+    // 这里可以添加其他需要的API
 }); 
